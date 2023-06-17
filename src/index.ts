e984931486919988cae1dab9c08356b84b8733e2
Richard Roxas <chardsourcex@gmail.com>
2023-06-17 20:04:02 +0800

    fix deployment

--- src/index.ts
+++ src/index.ts
@@ -39,18 +39,18 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    process.env.CLIENT_URL || "http://localhost:5173"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     process.env.CLIENT_URL || "http://localhost:5173"
+//   );
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.set("trust proxy", 1); // trust first proxy
 

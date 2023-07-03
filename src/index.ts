5dbec8e1dc7c296f58fb743cfc6672b5c6f8f759
Richard Roxas <chardsourcex@gmail.com>
2023-07-04 01:51:32 +0800

    fix deployment

--- src/index.ts
+++ src/index.ts
@@ -79,7 +79,6 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
-      domain: process.env.NODE_ENV === "production" ? "onrender.com" : "",
     },
   })
 );

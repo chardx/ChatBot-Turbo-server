c0e746cf6f7d042bffe12337d8f3a8134ccccedd
Richard Roxas <chardsourcex@gmail.com>
2023-07-09 19:37:55 +0800

    fix deployment

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -103,7 +103,7 @@ router.route("/google/callback").get(
     console.log("Google Callback");
     console.log(req.user);
     res.cookie("jwtToken", req.user.token, {
-      domain: "chadxgpt.online",
+      domain: ".online",
       httpOnly: false,
       secure: true,
       sameSite: "none",

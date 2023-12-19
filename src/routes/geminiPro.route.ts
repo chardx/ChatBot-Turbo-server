63f5e0e56bb27472d753d82194e24595c34b35d5
Richard Roxas <chardsourcex@gmail.com>
2023-12-19 13:03:02 +0800

    feat: added streaming function using Gemini Pro

--- src/routes/geminiPro.route.ts
+++ src/routes/geminiPro.route.ts
@@ -2,34 +2,48 @@ import express from "express";
 
 const router = express.Router();
 import { GoogleGenerativeAI } from "@google/generative-ai";
+
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 
-router.route("/").post(async (req, res) => {
-  const { chatMessages, activeAI } = req.body;
+router.route("/").get(async (req, res) => {
+  const rawChatMessages: any = req.query.chatMessages;
+  const rawActiveAI: any = req.query.activeAI;
+
+  const chatMessages = JSON.parse(rawChatMessages);
+  const activeAI = JSON.parse(rawActiveAI);
+  console.log(chatMessages);
+  console.log(activeAI);
+
   try {
-    const results = await runGeminiPro(req, chatMessages, activeAI);
-    res.status(200).json({ results });
+    const chunks = await runGeminiPro(
+      req,
+      chatMessages,
+      activeAI,
+      (chunkText) => {
+        console.log("Received chunk:", chunkText);
+        res.write(chunkText);
+      }
+    );
+    res.end();
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Internal server error." });
   }
+
+  res.on("close", () => {
+    console.log("Client closed connection");
+    res.end();
+  });
 });
 
-const runGeminiPro = async (req, chatMessages, activeAI) => {
+const runGeminiPro = async (req, chatMessages, activeAI, chunkCallback) => {
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
   const [systemMessage, history, userNewMessage] = getHistoryAndNewMessage(
     chatMessages,
     activeAI
   );
-  console.log("Value of system message");
-  console.log(systemMessage);
-
-  console.log("Value of history");
-  console.log(history);
   let modifiedHistory = [systemMessage, ...history];
-  console.log("HIstory dummy");
-  console.log(modifiedHistory);
   const chat = model.startChat({
     history: [
       {
@@ -43,15 +57,27 @@ const runGeminiPro = async (req, chatMessages, activeAI) => {
     },
   });
 
-  const result = await chat.sendMessage(userNewMessage);
-  const { response } = result;
-  const text = response.text();
+  //   const result = await chat.sendMessage(userNewMessage);
+  const result = await chat.sendMessageStream(userNewMessage);
+  console.log("RESULT");
+  console.log(result.stream);
+  for await (const chunk of result.stream) {
+    console.log(chunk);
+    console.log("CHUNK.TEXT");
+    console.log(chunk.text);
+    const chunkText = chunk.text();
+    if (!chunkText) {
+      console.log("BLANK DETECTED!");
+      return;
+    }
+    console.log(chunkText);
 
-  console.log(text);
-  return text;
+    chunkCallback(chunkText);
+  }
 };
 
 const getHistoryAndNewMessage = (chatMessages, activeAI) => {
+  console.log(activeAI);
   const systemMessage = { role: "model", parts: activeAI.content };
   const userNewMessage = chatMessages[chatMessages.length - 1].message;
   let history = chatMessages
